081d5db2150984da02f8c464b6184a75ed99e574
Adam Nagy <anagy@rapidminer.com>
2017-03-17 10:40:02 +0100

    working jwt authentication

---
+++ src/main/java/com/nagyadam2092/security/jwt/AccountCredentials.java
@@ -0,0 +1,19 @@
+package com.nagyadam2092.security.jwt;
+
+public class AccountCredentials {
+
+	    private String username;
+	    private String password;
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+}
---
+++ src/test/java/com/nagyadam2092/SpringBootVuejsJwtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nagyadam2092;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootVuejsJwtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
